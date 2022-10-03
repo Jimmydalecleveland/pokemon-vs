913dc9a12b6d4bf1eaa873097dcda0270b908370
Jimmy DC <jimmydalecleveland@gmail.com>
2022-10-03 15:27:34 -0600

    update pokemon page to handle invalid pokemon

--- pages/[pokemon].js
+++ pages/[pokemon].js
@@ -1,4 +1,12 @@
 export default function Pokemon({ pokemon }) {
+  if (pokemon.error) {
+    return (
+      <main>
+        <h1>{pokemon.error}</h1>
+      </main>
+    )
+  }
+
   return (
     <main>
       <h1>{pokemon.name}</h1>
@@ -23,8 +31,13 @@ export default function Pokemon({ pokemon }) {
 export async function getServerSideProps({ params }) {
 
   // Fetch pokemon from external API
-  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.pokemon}`)
-  const pokemon = await res.json()
+  const data = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.pokemon}`)
+    .then((res) => res.json())
+    .catch(err => console.log(err))
+
+  const pokemon = data || {
+    error: `No pokemon exists with the name ${params.pokemon}`,
+  }
 
   // Pass pokemon to the page via props
   return { props: { pokemon } }
